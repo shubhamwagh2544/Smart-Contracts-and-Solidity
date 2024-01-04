d258c9963b92640e3a89550ffc226e598a64fe78
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-04 20:11:17 +0530

    constructor, getter setter

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,10 +3,31 @@ pragma solidity ^0.8.9;
 
 contract SimpleStorage {
     //basic types : boolean, uint, int, address bytes
-    bool isTrue = true;
-    uint256 number = 88;
-    int digit = -88;
-    string name = "shubham wagh";
-    address myAddress = 0x0F23407b1fA015D42a9dAac5b1A1bf4A07cC60bB;
-    bytes byteNumber = "cat";
+    bool isTrue;
+    uint256 number;
+    int digit;
+    string name;
+    address myAddress;
+    bytes byteNumber;
+
+    //constructor
+    constructor() {
+        isTrue = true;
+        number = 88;
+        digit = -88;
+        name = "shubham wagh";
+        myAddress = 0x0F23407b1fA015D42a9dAac5b1A1bf4A07cC60bB;
+        byteNumber = "cat";
+    }
+
+    //setter
+    function set(uint _number) public {
+        number = _number;
+    } 
+
+    //getter
+    function get() public view returns(uint256) {
+        return number;
+    }
+
 }
