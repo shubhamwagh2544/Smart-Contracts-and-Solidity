048d6a2ea11632d7962c010cbcef733ddca4a623
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-04 18:22:31 +0530

    basic data types

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.9;
 
 contract SimpleStorage {
-    uint number = 10;
-    
+    //basic types : boolean, uint, int, address bytes
+    bool isTrue = true;
+    uint256 number = 88;
+    int digit = -88;
+    string name = "shubham wagh";
+    address myAddress = 0x0F23407b1fA015D42a9dAac5b1A1bf4A07cC60bB;
+    bytes byteNumber = "cat";
 }
