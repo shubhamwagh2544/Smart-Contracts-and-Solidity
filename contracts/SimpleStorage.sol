ae579f1d13857360ab6ed3f5feda14966b14b668
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-05 13:47:09 +0530

    struct, mapping, arrays

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.9;         //solidity version
 
 contract SimpleStorage {
     //basic types : boolean, uint, int, address bytes
@@ -9,6 +9,17 @@ contract SimpleStorage {
     string name;
     address myAddress;
     bytes byteNumber;
+
+    //struct
+    struct Person {
+        string myName;
+        uint256 myNumber;
+    }
+
+    Person[] public persons;
+
+    //mapping
+    mapping(string => uint256) public nameToNumberMapping;
 
     //constructor
     constructor() {
@@ -26,8 +37,15 @@ contract SimpleStorage {
     } 
 
     //getter
+    //view, pure
     function get() public view returns(uint256) {
         return number;
+    }
+
+    //memory, calldata, storage
+    function addPersonToList(string memory _name, uint256 _number) public {
+        persons.push(Person(_name, _number));
+        nameToNumberMapping[_name] = _number;
     }
 
 }
